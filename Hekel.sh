07a18c4558aedbd39b8eef0075eabbb25431bdce
MR_KALIT09 <75316440+MRKALIT09@users.noreply.github.com>
2021-01-07 14:49:49 +0700

    Add files via upload

---
+++ Hekel.sh
@@ -0,0 +1,107 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+sleep 2
+figlet "MR.KALIT09"
+sleep 2
+echo
+echo  $bi"[•]▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁[•]"$bi
+echo  $me"[•] $bi AUTHOR : MR.KALIT09$bi $me [•]"
+echo  $me" WHATSAPP ME: 0823-7780-4293"
+echo  $ku"MY GITHUB:https://github.com/.MRKALIT09"
+echo  $bi"[•]▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁$bi[•]"
+sleep 2
+echo $i"▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+echo $pur "MASUKAN MENU TOOLS SC NYA"
+sleep 2
+echo $i"▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+sleep 1
+echo $cy "1.MBF FB"
+sleep 2
+echo $cy "2.BOK3P"
+sleep 2
+echo $cy "3.SPAM-SMS"
+sleep 2
+echo $cy "4.CRACK NO LOGIN"
+sleep 2
+echo $cy "5.Pesan"
+sleep 2
+echo $cy "0.EXIT TOOLS"
+sleep 2
+echo $pi"▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+read -p  " [=====>] " pill
+#batas gan
+if [ $pill = "1" ]
+then
+sleep 1
+echo "SABAR YA TOL DIKIT LAGI "
+git clone https://github.com/MRKALIT09/MandoCrack12
+cd MandoCrack12
+python2 luangan.py
+sleep 1
+echo " Dadah Kontol "
+fi
+#batas gan
+if [ $pill = "2" ]
+then
+sleep 2
+echo "MAU COLI LU KAN KINTIL"
+git clone https:github.com/MRKALIT09/colikiawan
+cd colikiawan
+sh Tobat.sh
+sleep 2
+echo "Bokep Tersimpan"
+fi
+#batas gan
+if [ $pill = "3" ]
+then
+sleep 2
+echo "GANGGU BOCIL EPEP YOK"
+git clone https://github.com/MRKALIT09/tukang-ganggu
+cd tukang-ganggu
+python2 Spam.py
+sleep 2
+echo "Dah siap Ganggu Orang Push Rank?"
+fi
+#batas gan
+if [ $pill = "4" ]
+then
+sleep 2
+echo "SC CRACK SEMUA NEGARA NO LOGIN"
+git clone https://github.com/MRKALIT09/crackmassal
+cd crackmassal
+python2 crackmassal
+sleep 2
+echo "BALIK LAGI YA"
+fi
+#batas gan
+if [ $pill = "5" ]
+then
+sleep 1
+echo "Lu Tau Recode"
+sleep 2
+echo "Jangan Recode Ya Anjik"
+sleep 2
+echo "Gw Capek Bikin Bikin Sc Nya"
+sleep 2
+echo "Dan Lu Tinggal Maen Recode"
+sleep 2
+echo "Satu Pesan Buat Tukang Recode"
+sleep 2
+echo "Kontol.Kontol.Kontol.Kontol.Kontol"
+fi
+#batas gan
+if [ $pill = "0" ]
+then
+sleep 1
+echo "kalau Scrip Nya Rusak Ingat"
+sleep 1
+echo "NOMOR WA SAYA DI ATAS"
+exit
+fi
